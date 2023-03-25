1a5f4a52ef5f7bdc7135924e924538365d97a678
Avery Wong <avwong13@g.ucla.edu>
2023-03-25 02:30:54 -0700

    Update and rename classInheritanceExample.js to classInheritanceVsPrototypaChainInheritanceExample.js

--- JSDS-Implem/classInheritanceVsPrototypaChainInheritanceExample.js
+++ JSDS-Implem/classInheritanceVsPrototypaChainInheritanceExample.js
@@ -34,3 +34,35 @@ Output:
 > "son :  derived DerivedMethod"
 > "son's father: base BaseMethod"
 */
+
+function Base(a = "default a") {
+  this.a = a
+}
+
+Base.prototype.BaseMethod = function() {
+   console.log(this.a + ": " + "base BaseMethod")
+}
+
+function Derived(b = "default b") {
+  this.b = b
+  Base.call(this, b + "'s father")
+  Object.setPrototypeOf(Derived.prototype, Base.prototype)
+}
+
+Derived.prototype.DerivedMethod = function() {
+  console.log(this.b + " : " + " derived DerivedMethod")
+}
+
+let base = new Base("father")
+base.BaseMethod()
+
+let derived = new Derived("son")
+derived.DerivedMethod()
+derived.BaseMethod()
+
+/*
+Output
+> "father: base BaseMethod"
+> "son :  derived DerivedMethod"
+> "son's father: base BaseMethod"
+*/
